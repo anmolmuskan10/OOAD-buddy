4276add6505910e2c7aad22bdf868c04e708d42f
anmolmuskan10 <anmolmuskan10@gmail.com>
2026-05-03 03:06:32 +0500

    render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,3 +1,2 @@
 #!/usr/bin/env bash
 pip install -r requirements.txt
-pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.5.0/en_core_web_sm-3.5.0-py3-none-any.whl
